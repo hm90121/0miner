e32776793b0786eb90b1527c1a462bf08fa08e56
anish-squareops <68534207+anish-squareops@users.noreply.github.com>
2020-12-16 18:45:55 +0530

    resolved ingress issue

--- 0chain_setup/utility/local_k8s.sh
+++ 0chain_setup/utility/local_k8s.sh
@@ -3,7 +3,7 @@ read -p "Provide your external/host IP: " host_ip
 echo "This may take some time, please wait..."
 microk8s stop
 microk8s start
-microk8s enable cilium dns storage metallb
+microk8s enable ingress cilium dns storage metallb
 microk8s enable host-access:ip=${host_ip}
 echo "Enabled micr0k8s on your system"
 exit
